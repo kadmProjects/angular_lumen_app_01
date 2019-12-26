608ed92ff524260cf3dec41fa938e0f81bbfc5ed
kadmProjects <40564817+kadmProjects@users.noreply.github.com>
2019-12-26 23:53:14 +0530

    Commit

---
+++ frontend/src/services/interfaces/town.ts
@@ -0,0 +1,6 @@
+export class Town {
+    town_name: string;
+    country_name: string;
+    country_code: string;
+    iso_country_code: string;
+}
--- frontend/src/services/town.service.ts
+++ frontend/src/services/town.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { Town } from './interfaces/town';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TownService {
+    // Backend URL
+    private _url = environment.API_URL;
 
     constructor(private http: HttpClient) {}
+
+    addTown(townData) {
+        return this.http.post<Town>(this._url, townData);
+    }
 }
